c3fd9176d6efd6a33500ff0f444fa61f067a7c98
ivanthich <ivanthich2@gmail.com>
2019-09-28 12:01:13 -0700

    Forgot these files from the last commit.

---
+++ EduBot/src/test/java/xbot/edubot/rotation/HelpTurnLeftTest.java
@@ -0,0 +1,38 @@
+package xbot.edubot.rotation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import xbot.edubot.BaseDriveTest;
+import xbot.edubot.subsystems.drive.commands.TurnLeft90DegreesCommand;
+
+public class HelpTurnLeftTest extends BaseDriveTest{
+    @Test
+    public void From150(){
+       double result = TurnLeft90DegreesCommand.To90Angle(240); // 240 how degrees you are moving
+       assertEquals(-120, result, 0.001); // -120 is the goal
+
+    }
+
+    @Test
+    public void Opposite150(){
+        double result = TurnLeft90DegreesCommand.To90Angle(0); 
+        assertEquals(0, result, 0.001); 
+ 
+     }
+
+     @Test
+    public void Opposite370(){
+        double result = TurnLeft90DegreesCommand.To90Angle(370); 
+        assertEquals(-170, result, 0.001);
+ 
+     }
+
+     @Test
+     public void From370(){
+      double result = TurnLeft90DegreesCommand.To90Angle(360); 
+      assertEquals(-180, result, 0.001);
+
+   }
+}
